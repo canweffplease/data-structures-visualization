77356c93ee4f0dbbdf62c0e8b1df0b4448389e6c
Christopher Sarzen <87316434+canweffplease@users.noreply.github.com>
2023-12-10 16:06:07 -0500

    Add files via upload

---
+++ src/data-structures/BinaryTree.js
@@ -0,0 +1,20 @@
+class TreeNode {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+    //insert data into tree
+
+    //traverse tree, in order pre order post order
+
+    //other methods
+}
+
+export default BinaryTree;
